a297dd4c36584906f0d80b36e837a31731b05332
Terry (Romme) Felton <116994662+rommethecoder@users.noreply.github.com>
2025-01-17 12:39:05 -0600

    Implements reservation services

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { ReservationService } from './../reservation/reservation.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Reservation } from '../models/reservation';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit{
 
+  reservationForm: FormGroup = new FormGroup({});
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private reservationService: ReservationService){
+
+    }
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required],
+    })
+  }
+
+  onSubmit(){
+    if(this.reservationForm.valid){
+      let reservation: Reservation = this.reservationForm.value;
+      this.reservationService.addReservation(reservation);
+    }
+  }
 }
