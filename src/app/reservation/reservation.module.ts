d0d873dd8bb16671aaf39a0092cecd9503fa918b
Terry (Romme) Felton <116994662+rommethecoder@users.noreply.github.com>
2025-01-17 13:17:32 -0600

    Adds button functionality and implements update reservation

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HomeModule } from '../home/home.module';
+
 
 
 
@@ -14,7 +17,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule,
+    HomeModule
   ]
 })
 export class ReservationModule { }
